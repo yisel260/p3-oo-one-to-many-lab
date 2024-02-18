cb0a04f57c8417e00985b4d8dfb502ef42569a19
Yisel Cintora Vega <yiselcintoravega@gmail.com>
2024-02-18 01:51:49 -0500

    completed lab

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,5 +1,45 @@
 class Pet:
-    pass
+     
+     PET_TYPES = ["dog", "cat", "rodent", "bird", "reptile", "exotic"]
+     all =[]
+
+     def __init__ (self,name,pet_type, owner = None):
+          self.name = name
+          self.pet_type = pet_type
+          self.owner = owner
+          Pet.all.append(self)
+    
+    
+     @property
+     def pet_type(self):
+      return self._pet_type
+     @pet_type.setter
+     def pet_type(self, value):
+         if value in Pet.PET_TYPES :
+             self._pet_type = value
+         else: 
+           raise ValueError ("Invalid pet type")
+     
+
 
 class Owner:
-    pass
+    def __init__(self,name):
+         self.name = name
+
+    def pets(self):
+        return [pet for pet in Pet.all if pet.owner == self]
+    
+    def add_pet(self, pet):
+        if not isinstance(pet, Pet):
+            raise TypeError("Pet must be an instance of Pet class")
+        pet.owner = self
+
+    
+    
+    def get_sorted_pets(self):
+       pets = [pet for pet in Pet.all if pet.owner == self]
+       return sorted(pets, key=lambda pet: pet.name)
+
+
+
+
